e7ceadccb68c33beee6af7692aa78c2e36565998
Ali El-Ashram <ali.elashram@gmail.com>
2013-09-23 23:22:16 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogTracker::Application.config.secret_key_base = '77e04d05f2bc6cef1ba5e408b2817888245485f06cb89d4d9f1d2af3218f66951fba867d2c0b51bbef9797d12158a44f900ca3144874fa750cc48d88e0714637'
